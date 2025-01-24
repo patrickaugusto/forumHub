e359da474be72473b381b6a79b033a1f26703250
Patrick Augusto <trick0augusto@gmail.com>
2025-01-24 17:19:28 -0300

    feat: qualquer origem pode fazer requisições

--- src/main/java/br/com/forumhub/demo/config/web/CorsConfig.java
+++ src/main/java/br/com/forumhub/demo/config/web/CorsConfig.java
@@ -1,5 +1,6 @@
 package br.com.forumhub.demo.config.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("*")
                 .allowedMethods("POST", "GET", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
