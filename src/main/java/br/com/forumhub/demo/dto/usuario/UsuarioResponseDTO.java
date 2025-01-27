cd67261a29d1d71acb67978a3d4dd19ebd26d4ce
Patrick Augusto <trick0augusto@gmail.com>
2025-01-27 17:51:59 -0300

    feat: retornando lista de topicos no dto de usuario

--- src/main/java/br/com/forumhub/demo/dto/usuario/UsuarioResponseDTO.java
+++ src/main/java/br/com/forumhub/demo/dto/usuario/UsuarioResponseDTO.java
@@ -1,16 +1,25 @@
 package br.com.forumhub.demo.dto.usuario;
 
+import br.com.forumhub.demo.dto.topico.TopicoResponseDTO;
 import br.com.forumhub.demo.model.entities.Usuario;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public record UsuarioResponseDTO(
         Long id,
         String nome,
         String email,
-        String role
+        String role,
+        List<TopicoResponseDTO> topicosFeito
 ) {
     public UsuarioResponseDTO(Usuario usuario) {
         this(
-                usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getRole().toString()
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getEmail(),
+                usuario.getRole().toString(),
+                usuario.getTopicos().stream().map(TopicoResponseDTO::new).collect(Collectors.toList())
         );
     }
 }
